bb31aa51e5382b29bf21130b712d6a8978636ced
Gareth Hemsworth <gareth_hemsworth@waters.com>
2022-06-09 17:13:38 +0100

    Terraforming ECS cluster, service and task-def

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.0"
+}
+
+provider "aws" {
+  profile = "qa"
+  region  = "us-west-2"
+}
+
+resource "aws_ecs_cluster" "scalars_qa" {
+  name = "scalars-qa"
+
+  setting {
+    name  = "containerInsights"
+    value = "disabled"
+  }
+}
+
+resource "aws_ecs_task_definition" "scalars_qa" {
+  family                = "scalars-app-task"
+  container_definitions = file("../ecs-fargate/task-definition.json")
+}
+
+resource "aws_ecs_service" "scalars_qa" {
+  name            = "scalars-app"
+  cluster         = aws_ecs_cluster.scalars_qa.id
+  desired_count   = 2
+  task_definition = aws_ecs_task_definition.scalars_qa.arn
+}
